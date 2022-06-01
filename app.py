ba99d4899b37323cda80b91e98c11991c4576d57
Tevin Temu <32607464+Temutev@users.noreply.github.com>
2022-06-01 16:12:34 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, analysis,shop, clusmap
+
+st.set_page_config(layout="wide")
+
+
+apps = MultiApp()
+
+# Add all your application here
+
+apps.add_app("Home", home.app)
+apps.add_app("RFM Analysis",analysis.app)
+apps.add_app("Shop Mappings", shop.app)
+apps.add_app("Mapping Sale Hotspots", clusmap.app)
+
+# The main app
+apps.run()
