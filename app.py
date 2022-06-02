03bc7ecbd2f444e3c0f9e845c56938f4bd8f06be
Tevin Temu <32607464+Temutev@users.noreply.github.com>
2022-06-02 19:39:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ apps.add_app("Home", home.app)
 apps.add_app("RFM Analysis",analysis.app)
 apps.add_app("Shop Mappings", shop.app)
 apps.add_app("Mapping Sale Hotspots", clusmap.app)
-
+apps.add_app("Links",links.app)
 # The main app
 apps.run()
