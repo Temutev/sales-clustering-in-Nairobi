93a103ed1bfe2a4b2d80b15e3aa6a3888112fe48
Tevin Temu <32607464+Temutev@users.noreply.github.com>
2022-06-02 19:40:04 +0300

    Add files via upload

---
+++ apps/links.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+
+def app():
+    st.title("Links to Resources and Data used.")
+
+    st.header("Introduction")
+    st.markdown(
+        """
+    Here is a link  to the github repo : https://github.com/Temutev/sales-clustering-in-Nairobi
+    Here is a link to the data used for this project: https://www.kaggle.com/datasets/tevintemu/solutechclustering
+    """
+    )
